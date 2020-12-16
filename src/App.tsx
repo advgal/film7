5f53c7a3a65512087086fe2c04a063144d1c5cd0
Peter Reif <peter.reif@wien.gv.at>
2020-12-16 15:19:57 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const [filme, setFilme] = useState ([]);
+
+    useEffect (() => {
+        holFilme ();
+    }, []);
+
+    const holFilme=async ()=>{
+        const res = await fetch (
+            "https://api.themoviedb.org/3/discover/movie?api_key=d49416cd8a2e65767b5ac717906e3f63"
+        );
+        const json = await res.json ();
+        debugger;
+        let results = json.results;
+        setFilme (results);
+    }
+
+    const out=()=>{
+        return filme.map ((film:any) => (<li>{film.title}</li>));
+    }
+    return (
+        <div className="App">
+            <h1>Filme</h1>
+            <ul>
+                {
+                    out()
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
